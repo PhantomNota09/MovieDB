a328c6d0ae807c35a3df387aa8731e3f22a064bd
PhantomNota09 <srayala5@asu.edu>
2026-01-26 07:45:58 -0700

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Surya Rayala on 1/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
