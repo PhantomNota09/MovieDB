341955180c7b4322b1e1a2e721af7ac901b32636
PhantomNota09 <srayala5@asu.edu>
2026-02-04 14:34:44 -0700

    added collectionview

--- MovieDB/Controller/MovieListVC.swift
+++ MovieDB/Controller/MovieListVC.swift
@@ -13,21 +13,37 @@ class MovieListVC: UIViewController {
     
     // MARK: - Properties
     
-    var movieTableView: UITableView?
+    // Table View (Commented Out)
+//    var movieTableView: UITableView?
+    
+    // Collection View
+    var movieCollectionView: UICollectionView?
     var pageLabel: UILabel?
     
     /// View model managing movie data and business logic
     let viewModel = MovieViewModel()
     
-    /// Enum for table view configuration values
-    enum TableViewConfig {
-        static let rowHeight: CGFloat = 160
+    // Table View Configuration (Commented Out)
+//    /// Enum for table view configuration values
+//    enum TableViewConfig {
+//        static let rowHeight: CGFloat = 160
+//        static let numberOfSections = 1
+//    }
+    
+    /// Enum for collection view configuration values
+    enum CollectionViewConfig {
         static let numberOfSections = 1
     }
     
+    // Cell Identifiers (Commented Out)
+//    /// Enum for cell identifiers
+//    enum Identifiers {
+//        static let movieTableViewCell = "MovieTableViewCell"
+//    }
+    
     /// Enum for cell identifiers
     enum Identifiers {
-        static let movieTableViewCell = "MovieTableViewCell"
+        static let movieCollectionViewCell = "MovieCollectionViewCell"
     }
     
     // MARK: - Lifecycle Methods
@@ -41,7 +57,11 @@ class MovieListVC: UIViewController {
         
         setupTitleLabel()
         
-        setupTableView()
+        // Table View Setup (Commented Out)
+//        setupTableView()
+        
+        // Collection View Setup
+        setupCollectionView()
     }
 }
 
@@ -62,73 +82,150 @@ private extension MovieListVC {
         if let pageLabel = pageLabel{
             view.addSubview(pageLabel)
             NSLayoutConstraint.activate([
-                pageLabel.topAnchor.constraint(equalTo: view.topAnchor),
-                pageLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -670),
+                pageLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                pageLabel.heightAnchor.constraint(equalToConstant: 35),
                 pageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
                 pageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
             ])
         }
     }
     
-    /// Configures the table view delegate and data source
-    func setupTableView() {
-        movieTableView = UITableView()
-        movieTableView?.delegate = self
-        movieTableView?.dataSource = self
-        movieTableView?.register(MovieTableViewCell.self, forCellReuseIdentifier: Identifiers.movieTableViewCell)
-        movieTableView?.translatesAutoresizingMaskIntoConstraints = false
+    // Table View Setup (Commented Out)
+//    /// Configures the table view delegate and data source
+//    func setupTableView() {
+//        movieTableView = UITableView()
+//        movieTableView?.delegate = self
+//        movieTableView?.dataSource = self
+//        movieTableView?.register(MovieTableViewCell.self, forCellReuseIdentifier: Identifiers.movieTableViewCell)
+//        movieTableView?.translatesAutoresizingMaskIntoConstraints = false
+//
+//        if let tableView = movieTableView, let pageLabel = pageLabel {
+//            view.addSubview(tableView)
+//            NSLayoutConstraint.activate([
+//                tableView.topAnchor.constraint(equalTo: pageLabel.bottomAnchor, constant: -70),
+//                tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//                tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+//            ])
+//        }
+//    }
+    
+    /// Configures the collection view delegate and data source
+    func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
         
-        if let tableView = movieTableView, let pageLabel = pageLabel {
-            view.addSubview(tableView)
+        // Cell sizing
+        layout.itemSize = CGSize(width: 160, height: 220)
+        
+        // Vertical scroll direction
+        layout.scrollDirection = .vertical
+        
+        // Section insets for padding
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 15, bottom: 20, right: 15)
+        
+        // Spacing between items
+        layout.minimumInteritemSpacing = 10  // Horizontal spacing between items
+        layout.minimumLineSpacing = 15       // Vertical spacing between rows
+        
+        movieCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        movieCollectionView?.backgroundColor = .systemBackground
+        movieCollectionView?.delegate = self
+        movieCollectionView?.dataSource = self
+        movieCollectionView?.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: Identifiers.movieCollectionViewCell)
+        movieCollectionView?.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let collectionView = movieCollectionView, let pageLabel = pageLabel {
+            view.addSubview(collectionView)
+            
             NSLayoutConstraint.activate([
-                tableView.topAnchor.constraint(equalTo: pageLabel.bottomAnchor, constant: -70),
-                tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-                tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+                collectionView.topAnchor.constraint(equalTo: pageLabel.bottomAnchor, constant: 10),
+                collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
             ])
         }
     }
 }
 
-// MARK: - UITableViewDataSource
+// MARK: - UITableViewDataSource (Commented Out)
 
-extension MovieListVC: UITableViewDataSource {
+//extension MovieListVC: UITableViewDataSource {
+//
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return TableViewConfig.numberOfSections
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.numberOfMovies
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.movieTableViewCell, for: indexPath) as? MovieTableViewCell else {
+//            return UITableViewCell()
+//        }
+//
+//        guard let movie = viewModel.movie(at: indexPath.row) else {
+//            return cell
+//        }
+//
+//        configureCell(cell, with: movie)
+//
+//        return cell
+//    }
+//}
+
+// MARK: - UICollectionViewDataSource
+
+extension MovieListVC: UICollectionViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return TableViewConfig.numberOfSections
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return CollectionViewConfig.numberOfSections
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numberOfMovies
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.movieTableViewCell, for: indexPath) as? MovieTableViewCell else {
-            return UITableViewCell()
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifiers.movieCollectionViewCell, for: indexPath) as? MovieCollectionViewCell else {
+            return UICollectionViewCell()
         }
         
-        guard let movie = viewModel.movie(at: indexPath.row) else {
+        guard let movie = viewModel.movie(at: indexPath.item) else {
             return cell
         }
         
-        configureCell(cell, with: movie)
+        configureCollectionCell(cell, with: movie)
         
         return cell
     }
 }
 
-// MARK: - UITableViewDelegate
+// MARK: - UITableViewDelegate (Commented Out)
 
-extension MovieListVC: UITableViewDelegate {
+//extension MovieListVC: UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return TableViewConfig.rowHeight
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//
+//        guard let selectedMovie = viewModel.movie(at: indexPath.row) else {
+//            return
+//        }
+//
+//        navigateToMovieDetail(with: selectedMovie)
+//    }
+//}
+
+// MARK: - UICollectionViewDelegate
+
+extension MovieListVC: UICollectionViewDelegate {
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return TableViewConfig.rowHeight
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        guard let selectedMovie = viewModel.movie(at: indexPath.row) else {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let selectedMovie = viewModel.movie(at: indexPath.item) else {
             return
         }
         
@@ -136,18 +233,28 @@ extension MovieListVC: UITableViewDelegate {
     }
 }
 
+
 // MARK: - Cell Configuration
 
 private extension MovieListVC {
     
-    /// Configures a movie cell with the provided movie data
-    func configureCell(_ cell: MovieTableViewCell, with movie: MovieModel) {
-        // Using view model's formatting methods
-        cell.movieTitleLabel.text = viewModel.formatTitle(movie.title)
-        cell.movieScoreLabel.text = viewModel.formatPopularityScore(movie.popularityScore)
-        cell.movieReleaseLabel.text = viewModel.formatReleaseYear(movie.releaseYear)
-        
-        // Set poster image
+    // Table View Cell Configuration (Commented Out)
+//    /// Configures a movie cell with the provided movie data
+//    func configureCell(_ cell: MovieTableViewCell, with movie: MovieModel) {
+//        // Using view model's formatting methods
+//        cell.movieTitleLabel.text = viewModel.formatTitle(movie.title)
+//        cell.movieScoreLabel.text = viewModel.formatPopularityScore(movie.popularityScore)
+//        cell.movieReleaseLabel.text = viewModel.formatReleaseYear(movie.releaseYear)
+//
+//        // Set poster image
+//        cell.moviePosterImageView.image = UIImage(systemName: movie.imageName)
+//    }
+    
+    /// Configures a movie collection cell with the provided movie data
+    func configureCollectionCell(_ cell: MovieCollectionViewCell, with movie: MovieModel) {
+        cell.movieTitleLabel.text = movie.title
+        cell.movieScoreLabel.text = "Score: \(movie.popularityScore)"
+        cell.movieReleaseLabel.text = "Year: \(movie.releaseYear)"
         cell.moviePosterImageView.image = UIImage(systemName: movie.imageName)
     }
 }
---
+++ MovieDB/View/MovieCollectionViewCell.swift
@@ -0,0 +1,100 @@
+//
+//  MovieCollectionViewCell.swift
+//  MovieDB
+//
+//  Created by Surya Rayala on 2/4/26.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    // Image view for movie poster
+    var moviePosterImageView = UIImageView()
+    
+    // Label for movie title
+    var movieTitleLabel = UILabel()
+    
+    // Label for movie score
+    var movieScoreLabel = UILabel()
+    
+    // Label for movie release year
+    var movieReleaseLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Setup cell appearance with rounded corners and shadow
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 12
+        contentView.layer.masksToBounds = true
+        
+        // Add subtle border
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.systemGray5.cgColor
+        
+        // Setup image view
+        moviePosterImageView.contentMode = .scaleAspectFit
+        moviePosterImageView.tintColor = .systemGray
+        moviePosterImageView.backgroundColor = .systemGray6
+        moviePosterImageView.layer.cornerRadius = 8
+        moviePosterImageView.clipsToBounds = true
+        moviePosterImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Setup title label
+        movieTitleLabel.font = .systemFont(ofSize: 14, weight: .semibold)
+        movieTitleLabel.textColor = .label
+        movieTitleLabel.numberOfLines = 2
+        movieTitleLabel.textAlignment = .center
+        movieTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Setup score label
+        movieScoreLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        movieScoreLabel.textColor = .secondaryLabel
+        movieScoreLabel.numberOfLines = 1
+        movieScoreLabel.textAlignment = .center
+        movieScoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Setup release year label
+        movieReleaseLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        movieReleaseLabel.textColor = .secondaryLabel
+        movieReleaseLabel.numberOfLines = 1
+        movieReleaseLabel.textAlignment = .center
+        movieReleaseLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add subviews
+        contentView.addSubview(moviePosterImageView)
+        contentView.addSubview(movieTitleLabel)
+        contentView.addSubview(movieScoreLabel)
+        contentView.addSubview(movieReleaseLabel)
+        
+        // Setup constraints with improved spacing
+        NSLayoutConstraint.activate([
+            // Image view constraints - centered at top with more padding
+            moviePosterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            moviePosterImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            moviePosterImageView.widthAnchor.constraint(equalToConstant: 100),
+            moviePosterImageView.heightAnchor.constraint(equalToConstant: 120),
+            
+            // Title label constraints with better spacing
+            movieTitleLabel.topAnchor.constraint(equalTo: moviePosterImageView.bottomAnchor, constant: 12),
+            movieTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            movieTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            
+            // Score label constraints with improved spacing
+            movieScoreLabel.topAnchor.constraint(equalTo: movieTitleLabel.bottomAnchor, constant: 8),
+            movieScoreLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            movieScoreLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            
+            // Release label constraints with more bottom padding
+            movieReleaseLabel.topAnchor.constraint(equalTo: movieScoreLabel.bottomAnchor, constant: 4),
+            movieReleaseLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            movieReleaseLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            movieReleaseLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -12)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
